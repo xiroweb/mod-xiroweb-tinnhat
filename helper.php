5428d185f44f307a03ab6c8577cf3bf0d607d907
XiroWeb <xiroweb@gmail.com>
2020-08-23 10:16:09 +0700

    Upload code

---
+++ helper.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_xiroweb_tinnhat
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\String\StringHelper;
+
+$com_path = JPATH_SITE . '/components/com_content/';
+
+JLoader::register('ContentHelperRoute', $com_path . 'helpers/route.php');
+JModelLegacy::addIncludePath($com_path . 'models', 'ContentModel');
+
+/**
+ * Helper for mod_xiroweb_tinnhat
+ *
+ * @since  1.6
+ */
+abstract class ModXirowebTinnhatHelper
+{
+	/**
+	 * Get a list of articles from a specific category
+	 *
+	 * @param   \Joomla\Registry\Registry  &$params  object holding the models parameters
+	 *
+	 * @return  mixed
+	 *
+	 * @since  1.6
+	 */
+	public static function getList(&$params)
+	{
+		// Get an instance of the generic articles model
+		$articles = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
+
+		// Set application parameters in model
+		$app       = JFactory::getApplication();
+		$appParams = $app->getParams();
+		$articles->setState('params', $appParams);
+
+		$articles->setState('list.start', 0);
+		$articles->setState('filter.published', 1);
+
+		// Set the filters based on the module params
+		$articles->setState('list.limit', (int) $params->get('count', 0));
+		$articles->setState('load_tags', $params->get('show_tags', 0) || $params->get('article_grouping', 'none') === 'tags');
+
+		// Access filter
+		$access     = !JComponentHelper::getParams('com_content')->get('show_noauth');
+		$authorised = JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id'));
+		$articles->setState('filter.access', $access);
+
+		$included_articles = $params->get('id_article', '');
+		if ($included_articles)
+		{
+			$included_articles = explode("\r\n", $included_articles);
+			$articles->setState('filter.article_id', $included_articles);
+
+			// InClude
+			$articles->setState('filter.article_id.include', true);
+		}
+
+		$items = $articles->getItems();
+
+		// Display options
+		$show_date        = $params->get('show_date', 0);
+		$show_date_field  = $params->get('show_date_field', 'created');
+		$show_date_format = $params->get('show_date_format', 'Y-m-d H:i:s');
+		$show_category    = $params->get('show_category', 0);
+		$show_hits        = $params->get('show_hits', 0);
+		$show_author      = $params->get('show_author', 0);
+		$show_introtext   = $params->get('show_introtext', 0);
+		$introtext_limit  = $params->get('introtext_limit', 100);
+
+		// Find current Article ID if on an article page
+		$option = $app->input->get('option');
+		$view   = $app->input->get('view');
+
+		if ($option === 'com_content' && $view === 'article')
+		{
+			$active_article_id = $app->input->getInt('id');
+		}
+		else
+		{
+			$active_article_id = 0;
+		}
+
+		// Prepare data for display using display options
+		foreach ($items as &$item)
+		{
+			$item->slug    = $item->id . ':' . $item->alias;
+
+			/** @deprecated Catslug is deprecated, use catid instead. 4.0 */
+			$item->catslug = $item->catid . ':' . $item->category_alias;
+
+			if ($access || in_array($item->access, $authorised))
+			{
+				// We know that user has the privilege to view the article
+				$item->link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));
+			}
+			else
+			{
+				$menu      = $app->getMenu();
+				$menuitems = $menu->getItems('link', 'index.php?option=com_users&view=login');
+
+				if (isset($menuitems[0]))
+				{
+					$Itemid = $menuitems[0]->id;
+				}
+				elseif ($app->input->getInt('Itemid') > 0)
+				{
+					// Use Itemid from requesting page only if there is no existing menu
+					$Itemid = $app->input->getInt('Itemid');
+				}
+
+				$item->link = JRoute::_('index.php?option=com_users&view=login&Itemid=' . $Itemid);
+			}
+
+			// Used for styling the active article
+			$item->active      = $item->id == $active_article_id ? 'active' : '';
+			$item->displayDate = '';
+
+			if ($show_date)
+			{
+				$item->displayDate = JHtml::_('date', $item->$show_date_field, $show_date_format);
+			}
+
+			if ($item->catid)
+			{
+				$item->displayCategoryLink  = JRoute::_(ContentHelperRoute::getCategoryRoute($item->catid));
+				$item->displayCategoryTitle = $show_category ? '<a href="' . $item->displayCategoryLink . '">' . $item->category_title . '</a>' : '';
+			}
+			else
+			{
+				$item->displayCategoryTitle = $show_category ? $item->category_title : '';
+			}
+
+			$item->displayHits       = $show_hits ? $item->hits : '';
+			$item->displayAuthorName = $show_author ? $item->author : '';
+
+			if ($show_introtext)
+			{
+				$item->introtext = JHtml::_('content.prepare', $item->introtext, '', 'mod_articles_category.content');
+				$item->introtext = self::_cleanIntrotext($item->introtext);
+			}
+
+			$item->displayIntrotext = $show_introtext ? self::truncate($item->introtext, $introtext_limit) : '';
+			$item->displayReadmore  = $item->alternative_readmore;
+		}
+
+		return $items;
+	}
+
+	/**
+	 * Strips unnecessary tags from the introtext
+	 *
+	 * @param   string  $introtext  introtext to sanitize
+	 *
+	 * @return mixed|string
+	 *
+	 * @since  1.6
+	 */
+	public static function _cleanIntrotext($introtext)
+	{
+		$introtext = str_replace(array('<p>','</p>'), ' ', $introtext);
+		$introtext = strip_tags($introtext, '<a><em><strong>');
+		$introtext = trim($introtext);
+
+		return $introtext;
+	}
+
+	/**
+	 * Method to truncate introtext
+	 *
+	 * The goal is to get the proper length plain text string with as much of
+	 * the html intact as possible with all tags properly closed.
+	 *
+	 * @param   string   $html       The content of the introtext to be truncated
+	 * @param   integer  $maxLength  The maximum number of charactes to render
+	 *
+	 * @return  string  The truncated string
+	 *
+	 * @since   1.6
+	 */
+	public static function truncate($html, $maxLength = 0)
+	{
+		$baseLength = strlen($html);
+
+		// First get the plain text string. This is the rendered text we want to end up with.
+		$ptString = JHtml::_('string.truncate', $html, $maxLength, $noSplit = true, $allowHtml = false);
+
+		for ($maxLength; $maxLength < $baseLength;)
+		{
+			// Now get the string if we allow html.
+			$htmlString = JHtml::_('string.truncate', $html, $maxLength, $noSplit = true, $allowHtml = true);
+
+			// Now get the plain text from the html string.
+			$htmlStringToPtString = JHtml::_('string.truncate', $htmlString, $maxLength, $noSplit = true, $allowHtml = false);
+
+			// If the new plain text string matches the original plain text string we are done.
+			if ($ptString === $htmlStringToPtString)
+			{
+				return $htmlString;
+			}
+
+			// Get the number of html tag characters in the first $maxlength characters
+			$diffLength = strlen($ptString) - strlen($htmlStringToPtString);
+
+			// Set new $maxlength that adjusts for the html tags
+			$maxLength += $diffLength;
+
+			if ($baseLength <= $maxLength || $diffLength <= 0)
+			{
+				return $htmlString;
+			}
+		}
+
+		return $html;
+	}
+
+}
---
+++ tmpl/default.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_xiroweb_tinnhat
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+<div class="module<?php echo $moduleclass_sfx; ?> mod-tinnhat-<?php echo $stylecss; ?>">
+	<?php foreach ($list as $item) : ?>
+				<?php $images = json_decode($item->images);
+				 ?>
+			
+				<?php if (!empty($images->image_intro)): ?>
+					<a class="box-tinnhat-title <?php echo $item->active; ?>" href="<?php echo $item->link; ?>">
+						<img width="100%" src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>" />
+					</a>
+				<?php endif;// if ($images->image_intro != ''): ?>
+					<<?php echo $params->get('title_heading', 'h1'); ?>>
+						<a class="box-tinnhat-title <?php echo $item->active; ?>" href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a>
+					</<?php echo $params->get('title_heading', 'h1'); ?>>
+					<div>
+				<?php if ($item->displayHits) : ?>
+					<span class="box-tinnhat-hits">
+					<?php echo JText::_('MOD_XIROWEB_TINNHAT_LUOTXEM'); ?><?php echo $item->displayHits; ?>
+					</span>
+				<?php endif; ?>
+
+				<?php if ($params->get('show_author')) : ?>
+					<span class="box-tinnhat-writtenby">
+					<?php echo JText::_('MOD_XIROWEB_TINNHAT_AUTHORNAME'); ?><?php echo $item->displayAuthorName; ?>
+					</span>
+				<?php endif; ?>
+
+				<?php if ($item->displayCategoryTitle) : ?>
+					<span class="box-tinnhat-category">
+					<?php echo JText::_('MOD_XIROWEB_TINNHAT_CATEGORYTITLE'); ?><?php echo $item->displayCategoryTitle; ?>
+					</span>
+				<?php endif; ?>
+
+				<?php if ($item->displayDate) : ?>
+					<span class="box-tinnhat-date">
+						<?php echo $item->displayDate; ?>
+					</span>
+				<?php endif; ?>
+
+				<?php if ($params->get('show_tags', 0) && $item->tags->itemTags) : ?>
+					<div class="box-tinnhat-tags">
+					<?php echo JText::_('MOD_XIROWEB_TINNHAT_TAG'); ?><?php echo JLayoutHelper::render('joomla.content.tags', $item->tags->itemTags); ?>
+					</div>
+				<?php endif; ?>
+				</div>
+
+				<?php if ($params->get('show_introtext')) : ?>
+					<p class="box-tinnhat-introtext">
+						<?php echo $item->displayIntrotext; ?>
+					</p>
+				<?php endif; ?>
+
+				<?php if ($params->get('show_readmore')) : ?>
+					<p class="box-tinnhat-readmore">
+						<a class="box-tinnhat-title <?php echo $item->active; ?>" href="<?php echo $item->link; ?>">
+							<?php if ($item->params->get('access-view') == false) : ?>
+								<?php echo JText::_('MOD_XIROWEB_TINNHAT_REGISTER_TO_READ_MORE'); ?>
+							<?php elseif ($readmore = $item->alternative_readmore) : ?>
+								<?php echo $readmore; ?>
+								<?php echo JHtml::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+							<?php elseif ($params->get('show_readmore_title', 0) == 0) : ?>
+								<?php echo JText::sprintf('MOD_XIROWEB_TINNHAT_READ_MORE_TITLE'); ?>
+							<?php else : ?>
+								<?php echo JText::_('MOD_XIROWEB_TINNHAT_READ_MORE_TITLE'); ?>
+								<?php echo JHtml::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+							<?php endif; ?>
+						</a>
+					</p>
+				<?php endif; ?>	
+		<?php endforeach; ?>
+</div>
